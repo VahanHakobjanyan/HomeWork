a683784d59ffba2a2b96dba3ec51844c774136ab
choptankw <91840361+choptankw@users.noreply.github.com>
2024-07-25 02:35:18 +0400

    MemberwiseClone

---
+++ MemberwiseClone/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberwiseClone
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public byte Age { get; set; }
+        public Eye Eye { get; set; }
+        public Person(string name, byte age, Eye eye)
+        {
+            Name = name;
+            Age = age;
+            Eye = eye;  
+        }
+
+        public Person MemberwiseCopy()
+        {
+            return this.MemberwiseClone() as Person;
+        }
+
+        public void PrintPerson()
+        {
+            Console.WriteLine(Name + " " + Age);
+        }
+    }
+
+    class Eye 
+    {
+        public string color { get; set; }
+    }
+
+}
---
+++ MemberwiseClone/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MemberwiseClone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("John", 28, new Eye());
+            Person person2 = person1.MemberwiseCopy();
+
+            person1.PrintPerson();
+            person2.PrintPerson();
+
+            Console.WriteLine(ReferenceEquals(person1.Eye, person2.Eye));
+        }
+    }
+}
+ 
