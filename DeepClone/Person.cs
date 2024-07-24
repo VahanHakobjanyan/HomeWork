a72e63158430e1239ced6ddd1224b74af74c3d83
choptankw <91840361+choptankw@users.noreply.github.com>
2024-07-25 03:08:58 +0400

    DeepClone

---
+++ DeepClone/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepClone
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public byte Age { get; set; }
+        public Eye Eye { get; set; }
+        public Person(string name, byte age, Eye eye)
+        {
+            Name = name;
+            Age = age;
+            Eye = eye;
+        }
+        private Person(Person sourcePerson)
+        {
+            Name = sourcePerson.Name;
+            Age = sourcePerson.Age;
+            Eye = new Eye() { color = sourcePerson.Eye.color };
+        }
+
+        public Person DeepCopy()
+        {
+            return new Person(this);
+        }
+
+        public void PrintPerson()
+        {
+            Console.WriteLine(Name + " " + Age);
+        }
+    }
+    class Eye
+    {
+        public string color { get; set; }
+    }
+}
---
+++ DeepClone/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DeepClone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("John", 25, new Eye());
+            Person person2 = person1.DeepCopy();
+
+            person1.PrintPerson();
+            person2.PrintPerson();
+
+            Console.WriteLine(ReferenceEquals(person1.Eye, person2.Eye));
+        }
+    }
+}
