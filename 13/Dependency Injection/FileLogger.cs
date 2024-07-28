2da66cef71ab762b199c3e3904800e4aa0a834fa
choptankw <91840361+choptankw@users.noreply.github.com>
2024-07-28 17:48:07 +0400

    fix last task

--- 13/Dependency Injection/FileLogger.cs
+++ 13/Dependency Injection/FileLogger.cs
@@ -8,13 +8,24 @@ namespace Dependency_Injection
 {
     internal class FileLogger : ILogger
     {
-        string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "TestLog.txt";
 
         public void Log(string message)
         {
+            //creating or overwriting existing file
             using (StreamWriter writer = File.CreateText(path))
             {
                 writer.WriteLine(message);
+            }
+
+            //opening file in console
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
             }
         }
     }
