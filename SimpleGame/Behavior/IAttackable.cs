72d2454d65275eb0b3df009350123c7bd94a5854
choptankw <91840361+choptankw@users.noreply.github.com>
2024-08-16 18:36:21 +0400

    Revert "Delete SimpleGame directory"
    This reverts commit c29afbb0df842efb041316b119d6fcc1e646cdc0.

---
+++ SimpleGame/Behavior/IAttackable.cs
@@ -0,0 +1,14 @@
+﻿using SimpleGame.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGame.Behavior
+{
+    internal interface IAttackable
+    {
+        void Attack(GameObject target);
+    }
+}
