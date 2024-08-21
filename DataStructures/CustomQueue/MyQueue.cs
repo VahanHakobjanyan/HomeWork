ca48a5840f5fa23be97fdec40dd61e6f68efe474
choptankw <91840361+choptankw@users.noreply.github.com>
2024-08-21 21:05:41 +0400

    Custom queue implementation

---
+++ DataStructures/CustomQueue/MyQueue.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomQueue
+{
+    internal class MyQueue<T> : IEnumerable<T>
+    {
+        public MyQueue() : this(0) { }
+
+        public MyQueue(int capacity)
+        {
+            if (capacity < 0)
+                throw new ArgumentException("Capacity should be bigger than zero");
+
+            _data = new T[capacity];
+            Count = _data.Length;
+            _head = default(T);
+            _tail = default(T);
+        }
+
+        public MyQueue(ICollection<T> collection)
+        {
+            if (collection == null)
+                throw new ArgumentNullException();
+
+            Count = collection.Count;
+            _data = new T[Count];
+            _capacity = Count;
+            collection.CopyTo(_data, 0);
+            _head = collection.ElementAt(0);
+            _tail = collection.ElementAt(Count - 1);
+        }
+
+        private T[] _data;
+        private int _capacity;
+        public int Count { get; private set; }
+        private T _head;
+        private T _tail;
+
+        public void Enqueue(T item)
+        {
+            if (_capacity <= Count)
+            {
+                ManageCapacity();
+            }
+
+            _data[Count] = item;
+            Count++;
+            UpdateTail();
+        }
+
+        public T Dequeue()
+        {
+            if (Count == 0)
+                throw new System.ArgumentException("Empty queue");
+
+            T itemToReturn = _head;
+            _data[0] = default(T);
+
+            for (int i = 0; i < Count; i++)
+            {
+                _data[i] = _data[i + 1];
+            }
+
+            _data[Count - 1] = default(T);
+            Count--;
+            _head = _data[0];
+
+            return itemToReturn;
+        }
+
+        public T Peek()
+        {
+            return _head;
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+            _data = new T[Count];
+            _head = default(T);
+            _tail = default(T);
+        }
+
+        public bool Contains(T item)
+        {
+            return _data.Contains(item);
+        }
+
+        private void ManageCapacity()
+        {
+            if (_capacity <= Count)
+            {
+                T[] newData = new T[_capacity * 2];
+                _capacity *= 2;
+                _data.CopyTo(newData, 0);
+                _data = newData;
+            }
+        }
+
+        private void UpdateTail()
+        {
+            if (Count == 1)
+                _tail = _head;
+            else
+                _tail = _data[Count - 1];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+                yield return _data[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
+
---
+++ DataStructures/CustomQueue/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+
+namespace CustomQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 4, 2, 84, 43 };
+            var myQ = new MyQueue<int>(arr);
+
+            Console.WriteLine("Whole queue");
+            foreach (int i in myQ)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("Whole queue after enquing 432");
+
+            myQ.Enqueue(432);
+            foreach (int i in myQ)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("Dequeued item - {0}", myQ.Dequeue());
+            Console.WriteLine("-----------------------------------------------------");
+
+            Console.WriteLine("After dequing");
+            foreach (int i in myQ)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("-----------------------------------------------------");
+
+            Console.WriteLine("First in queue - {0}", myQ.Peek());
+
+            Console.WriteLine("-----------------------------------------------------");
+
+            Console.WriteLine("Contains 84 - {0}", myQ.Contains(84));
+            Console.WriteLine("Contains 99 - {0}", myQ.Contains(99));
+
+            Console.WriteLine("-----------------------------------------------------");
+
+            myQ.Clear();
+            Console.WriteLine("Queue is empty after clearing");
+            foreach (int i in myQ)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
