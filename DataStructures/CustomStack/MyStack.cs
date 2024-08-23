c9672486d45cf1567adbaafb9948b5ebceafa798
choptankw <91840361+choptankw@users.noreply.github.com>
2024-08-23 15:07:33 +0400

    Custom Stack Implementation

---
+++ DataStructures/CustomStack/MyStack.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    internal class MyStack<T> : IEnumerable<T>
+    {
+        public MyStack() : this(0) { }
+        public MyStack(int capacity)
+        {
+            if (capacity < 0)
+                throw new ArgumentOutOfRangeException(nameof(capacity));
+
+            Count = 0;
+            _capacity = capacity;
+            _data = new T[capacity];
+        }
+        public MyStack(ICollection<T> collection)
+        {
+            Count = collection.Count;
+            _capacity = Count;
+            _data = new T[Count];
+            collection.CopyTo(_data, 0);
+        }
+
+        private T[] _data;
+        private int _capacity;
+        public int Count { get; private set; }
+
+        public void Push(T item)
+        {
+            if (_capacity <= Count)
+            {
+                ManageCapacity();
+            }
+
+            _data[Count] = item;
+            Count++;
+        }
+
+        public T Pop()
+        {
+            if (Count == 0)
+                throw new System.ArgumentException("Empty stack");
+
+
+            T itemToReturn = _data[Count - 1];
+            _data[Count - 1] = default(T);
+            Count--;
+            return itemToReturn;
+
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+                throw new System.ArgumentException("Empty stack");
+
+            return _data[Count - 1];
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_data[i].Equals(item))
+                    return true;
+            }
+            return false;
+        }
+        private void ManageCapacity()
+        {
+            if (_capacity <= Count)
+            {
+                T[] newData = new T[_capacity * 2];
+                _capacity *= 2;
+                _data.CopyTo(newData, 0);
+                _data = newData;
+            }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                yield return _data[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ DataStructures/CustomStack/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace CustomStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 4, 1 };
+            MyStack<int> myS = new MyStack<int>(arr);
+
+            myS.Push(1);
+            myS.Push(4);
+            myS.Push(5);
+            myS.Push(65);
+
+            foreach (int i in myS)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("------------------------------------------------------");
+
+            myS.Pop();
+
+            foreach (int i in myS)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("------------------------------------------------------");
+
+            Console.WriteLine(myS.Peek());
+
+            Console.WriteLine("------------------------------------------------------");
+
+            Console.WriteLine(myS.Contains(0));
+            Console.WriteLine(myS.Contains(5));
+
+        }
+    }
+}
